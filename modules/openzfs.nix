67242cde4c5005a219dde0c117c78042cecd195e
davralin <gpg@davralin.work>
2026-06-11 16:26:24 +0200

    fix: set boot.zfs.forceImportRoot = false for 26.11 compatibility

--- modules/openzfs.nix
+++ modules/openzfs.nix
@@ -3,6 +3,7 @@
 {
   # Add zfs-support
   boot.supportedFilesystems = [ "zfs" ];
+  boot.zfs.forceImportRoot = false;
   services.zfs.autoSnapshot.enable = true;
   services.zfs.autoSnapshot.flags = "-p --utc";
   services.zfs.autoScrub.enable = true;
